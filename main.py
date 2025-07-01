f72f9fc3981d1aa2d693f862003532630dd08e6d
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2025-07-01 06:41:37 +0300

    feat: refactor content generation to use structured, stored messages

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
     args = sys.argv[1:]
-
     if not args:
         print("AI Code Assistant")
         print('\nUsage: python main.py "your prompt here"')
@@ -18,12 +18,22 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages)
+
+
+def generate_content(client, messages):
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001", contents=user_prompt
+        model="gemini-2.0-flash-001", contents=messages
     )
+
     print("Prompt tokens:", response.usage_metadata.prompt_token_count)
     print("Response tokens:", response.usage_metadata.candidates_token_count)
-    print(f"Response:\n{response.text}")
+    print("Response:")
+    print(response.text)
 
 
 if __name__ == "__main__":
