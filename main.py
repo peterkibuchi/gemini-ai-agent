5613622faabceadee892a76b4c1736cf33f8dec0
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2025-07-01 13:41:06 +0300

    feat: enhance argument handling and add verbose flag for user prompt

--- main.py
+++ main.py
@@ -6,13 +6,21 @@ from google.genai import types
 
 
 def main():
-    args = sys.argv[1:]
+    args: list[str] = sys.argv[1:]
     if not args:
         print("AI Code Assistant")
         print('\nUsage: python main.py "your prompt here"')
         print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
-    user_prompt = " ".join(args)
+
+    user_prompt: str = ""
+    verbose: bool = False
+
+    if args[-1] == "--verbose":
+        user_prompt = " ".join(args[:-1])
+        verbose = True
+    else:
+        user_prompt = " ".join(args)
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -22,16 +30,18 @@ def main():
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    generate_content(client, messages)
+    generate_content(client, messages, user_prompt, verbose)
 
 
-def generate_content(client, messages):
+def generate_content(client, messages, user_prompt, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001", contents=messages
     )
 
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    if verbose:
+        print("User prompt:", user_prompt)
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
     print("Response:")
     print(response.text)
 
