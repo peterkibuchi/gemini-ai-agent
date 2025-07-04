e927f04c67716f183dad2707edb3b823e9903cff
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2025-07-04 12:57:01 +0300

    feat: implement `run_python_file` function and update tests for Python file execution

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 from functions.get_file_content import get_file_content
 from functions.get_files_info import get_files_info
-from functions.write_file import write_file
+from functions.write_file_content import write_file
+from functions.run_python import run_python_file
 
 
 def test():
@@ -28,20 +29,32 @@ def test():
     # print("Calling get_file_content w/ ('calculator', 'pkg/calculator.py'):")
     # print(get_file_content("calculator", "pkg/calculator.py"), "\n")
 
-    # print("Calling get_fileget_file_contents_info w/ ('calculator', '/bin/cat'):")
+    # print("Calling get_file_content w/ ('calculator', '/bin/cat'):")
     # print(get_file_content("calculator", "/bin/cat"), "\n")
 
-    print("Calling write_file w/ ('calculator', 'lorem.txt', 'wait, this isn't lorem ipsum'):")
-    print(write_file("calculator", "lorem.txt",
-          "wait, this isn't lorem ipsum"), "\n")
+    # print("Calling write_file w/ ('calculator', 'lorem.txt', 'wait, this isn't lorem ipsum'):")
+    # print(write_file("calculator", "lorem.txt",
+    #       "wait, this isn't lorem ipsum"), "\n")
 
-    print("Calling write_file w/ ('calculator', 'pkg/morelorem.txt', 'lorem ipsum dolor sit amet'):")
-    print(write_file("calculator", "pkg/morelorem.txt",
-          "lorem ipsum dolor sit amet"), "\n")
+    # print("Calling write_file w/ ('calculator', 'pkg/morelorem.txt', 'lorem ipsum dolor sit amet'):")
+    # print(write_file("calculator", "pkg/morelorem.txt",
+    #       "lorem ipsum dolor sit amet"), "\n")
 
-    print("Calling write_file w/ ('calculator', '/tmp/temp.txt', 'this should not be allowed'):")
-    print(write_file("calculator", "/tmp/temp.txt",
-          "this should not be allowed"), "\n")
+    # print("Calling write_file w/ ('calculator', '/tmp/temp.txt', 'this should not be allowed'):")
+    # print(write_file("calculator", "/tmp/temp.txt",
+    #       "this should not be allowed"), "\n")
+
+    print("Calling run_python_file w/ ('calculator', 'main.py'):")
+    print(run_python_file("calculator", "main.py"), "\n")
+
+    print("Calling run_python_file w/ ('calculator', 'tests.py'):")
+    print(run_python_file("calculator", "tests.py"), "\n")
+
+    print("Calling run_python_file w/ ('calculator', '../main.py'):")
+    print(run_python_file("calculator", "../main.py"), "\n")
+
+    print("Calling run_python_file w/ ('calculator', 'nonexistent.py'):")
+    print(run_python_file("calculator", "nonexistent.py"), "\n")
 
 
 if __name__ == "__main__":
